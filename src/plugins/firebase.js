3345681894f78e624ce7458f9594e3c9e64db1d8
長内拓史 <84653168+tkm-webwork@users.noreply.github.com>
2021-08-10 16:57:50 +0900

    firebaseの情報だけ削除

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,14 +3,14 @@ import firebase from "firebase";
 // 以下の値は各々設定して使用してください。
 
 let firebaseConfig = {
-  apiKey: "AIzaSyARdaJXyULB6kS-KEAvB9NtJlPgli8wKfg",
-  authDomain: "ramen-chat.firebaseapp.com",
-  databaseURL: "https://ramen-chat-default-rtdb.firebaseio.com",
-  projectId: "ramen-chat",
-  storageBucket: "ramen-chat.appspot.com",
-  messagingSenderId: "835985987733",
-  appId: "1:835985987733:web:71fb9c8a23a9ec0c76639c",
-  measurementId: "G-3MWPQC71HK"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
